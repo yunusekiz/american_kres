75ff7c3e8b9e6ad8bf334f14fbe53f8be50807d7
Yunus Ekiz <ynsekiz@gmail.com>
2013-11-06 20:11:33 +0200

    ara commit

--- application/views/frontend_views/iletisim_view.php
+++ application/views/frontend_views/iletisim_view.php
@@ -20,7 +20,7 @@
 <link href="custom.css" media="screen" rel="stylesheet">
 <!-- main JS libs  -->
 <script src="js/libs/modernizr.min.js"></script>
-<script src="js/libs/respond.min.js"></script>					 
+<script src="js/libs/respond.min.js"></script>           
 <script src="js/libs/jquery.min.js"></script>
 <!-- scripts  -->
 <script src="js/jquery.spritely.min.js"></script>
@@ -38,12 +38,12 @@
 
 <body>
 <div class="body_wrap">
-	
+  
   <!-- header -->
   <div class="header">
       <div class="container">
       
-      	  <div class="sunbeam"></div>
+          <div class="sunbeam"></div>
           <div class="rocket"></div>
           
           <div class="logo">
@@ -84,81 +84,81 @@
   </div>
   <div id="top_clouds" class="top_stage"></div>
   <!--/ header -->
-		
+    
 <!-- middle -->
 <div id="middle" class="full_width">
-	<div class="container clearfix">
-    	
+  <div class="container clearfix">
+      
         <div id="mid_bird"></div>
         <div id="mid_baloon"></div>
-    	
+      
         <div class="content_wrapper">   
-        	
+          
             <div class="title_top">
-            	<h1>İletişim</h1>
+              <h1>İletişim</h1>
             </div>
             
-	        <!-- content -->
-	        <div class="content">
-            	
+          <!-- content -->
+          <div class="content">
+              
                 <!-- contact form -->
                 <div class="box_sprite">
-                	<div class="box_sprite_inner clearfix">
-                    	
-                        <div class="col_left">                        	
-                        	<!-- contact form -->
+                  <div class="box_sprite_inner clearfix">
+                      
+                        <div class="col_left">                          
+                          <!-- contact form -->
                             <div class="contact_form">
-			                <form action="iletisim/mesaj" method="post" id="contactForm">		                    
-			                    <div class="row field_text field_name">	                        
-			                        <input type="text" name="name" value="" class="inputtext input_middle required" placeholder="İsim,Soyisim" required>
-			                    </div>
-			                    <div class="clear"></div>                                
-			                    <div class="row field_text field_email">
-			                        <input type="text" name="email" value="" class="inputtext input_middle required" placeholder="E-posta" required>
-			                    </div>		                    
-			                    <div class="clear"></div>  
-			                    <div class="row field_message">
-			                        <textarea cols="30" rows="10" name="message" class="textarea textarea_middle required" placeholder="Mesaj..." required></textarea>
-			                    </div>
-			                    
-			                    <div class="row rowSubmit clearfix">
-			                        <input type="submit" value="Gönder" class="btn-submit">
-			                    </div>
-			                </form>
+                      <form action="iletisim/mesaj" method="post" id="contactForm">                       
+                          <div class="row field_text field_name">                         
+                              <input type="text" name="name" value="" class="inputtext input_middle required" placeholder="İsim,Soyisim" required>
+                          </div>
+                          <div class="clear"></div>                                
+                          <div class="row field_text field_email">
+                              <input type="text" name="email" value="" class="inputtext input_middle required" placeholder="E-posta" required>
+                          </div>                        
+                          <div class="clear"></div>  
+                          <div class="row field_message">
+                              <textarea cols="30" rows="10" name="message" class="textarea textarea_middle required" placeholder="Mesaj..." required></textarea>
+                          </div>
+                          
+                          <div class="row rowSubmit clearfix">
+                              <input type="submit" value="Gönder" class="btn-submit">
+                          </div>
+                      </form>
                             </div>
                             <!--/ contact form -->
                         </div>
                         
                         <div class="col_right">
-                        	<h2>irtibat bilgilerimiz</h2>
+                          <h2>irtibat bilgilerimiz</h2>
                             <div class="subtitle">American Fine Arts and Drama Anaokulları</div>
                             
                             <div class="contact_info">
                               {contact_iteration}
-								<div class="row address">{address}</div>						
-		                        <div class="row phone"><em>Telefon:</em> <span> {phone} </span></div>
-		                        <div class="row fax"><em>Faks:</em> <span> {fax} </span></div>
+                <div class="row address">{address}</div>            
+                            <div class="row phone"><em>Telefon:</em> <span> {phone} </span></div>
+                            <div class="row fax"><em>Faks:</em> <span> {fax} </span></div>
                                 <!-- <div class="row mail"><em>E-Posta:</em> <a href="mailto:contact@company.com">contact@company.com</a></div> -->
-							               {/contact_iteration}
+                             {/contact_iteration}
                            </div>
                             
                         </div>
                     </div>
-	            </div>
-  				<!--/ contact form -->
+              </div>
+          <!--/ contact form -->
                 
                 <div class="location_map">
-                	<h2>Harita:</h2>
+                  <h2>Harita:</h2>
                     
                     <div class="gmap">
-                    	<div id="gmap"></div>
+                      <div id="gmap"></div>
                     </div>
           <script>
               $(window).ready(function () {
                   $("#gmap").gMap({
                       markers: [{
-                      latitude: 39.880438,
-                      longitude: 32.821053,
+                      latitude: 39.883618,
+                      longitude: 32.821135,
                       title: "American Fine Arts and Drama Anaokulları",
                       html:"<strong> American Fine Arts and Drama Anaokulları </strong> <br>Adres: Huzur Mahallesi 1066. Cadde (Eski 1. Cadde) No:29/1 Öveçler / ANKARA",
                       popup: false,
@@ -176,15 +176,15 @@
           </script>
                 </div>
                 
-	        </div>
-	        <!--/ content -->
-		                    
+          </div>
+          <!--/ content -->
+                        
           <div class="clear"></div>
         </div>
         
         
       <div id="bottom_bee"></div>        
-	</div>
+  </div>
 </div>        
 <!--/ middle -->
 
@@ -192,13 +192,13 @@
 
 <div class="footer">
 <div class="inner">
-	<div class="container clearfix">
+  <div class="container clearfix">
     
         <div class="copyright">
-        	<span class="copy">2013</span>  
-        	<a href="">American Fine Arts and Drama Anaokulları</a>
+          <span class="copy">2013</span>  
+          <a href="">American Fine Arts and Drama Anaokulları</a>
         </div>
-        	        
+                  
     </div> 
 </div>
 </div>
